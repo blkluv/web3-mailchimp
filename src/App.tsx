f8905388e3e3614b8b2ca9e01616e546c72dd317
Johanna Fransson <adrina0002@gmail.com>
2023-09-21 14:12:12 -0500

    add xmtp as provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Email from "./pages/Email";
 import Home from "./pages/Home";
 import "./styles/Home.css";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 const About = () => <h2>About</h2>;
-const Email = () => <h2>Email</h2>;
 
 export default function App() {
   return (
